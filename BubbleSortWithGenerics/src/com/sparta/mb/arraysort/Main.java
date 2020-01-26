5ef49c1c6ebdda2e2ccb454d953590dca19c2f56
Mehmet Bekiroglu <mehmet.bekiroglu@outlook.com>
2020-01-26 17:19:00 +0000

    Add files via upload

--- BubbleSortWithGenerics/src/com/sparta/mb/arraysort/Main.java
+++ BubbleSortWithGenerics/src/com/sparta/mb/arraysort/Main.java
@@ -7,7 +7,10 @@ public class Main {
     public static void main(String[] args) {
         // write your code here
         Integer[] a = {3, 8, 6, 7, 4, 8};
+        String[] b = {"a", "c", "b"};
         Bubble bubble = new Bubble(a);
         System.out.println(Arrays.toString(bubble.arraySort((bubble.getArray())))) ;
+        Bubble bubble1 = new Bubble(b);
+        System.out.println(Arrays.toString(bubble1.arraySort((bubble1.getArray())))) ;
     }
 }
